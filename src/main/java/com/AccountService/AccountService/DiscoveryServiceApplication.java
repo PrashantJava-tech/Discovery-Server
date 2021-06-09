32c6d07ddce471641231fbcb46b72848ed2665bb
PrashantJava-tech <prashant.kdm29@gmail.com>
2021-06-09 21:49:25 +0530

    Name & yml file update

--- src/main/java/com/AccountService/AccountService/DiscoveryServiceApplication.java
+++ src/main/java/com/AccountService/AccountService/DiscoveryServiceApplication.java
@@ -6,11 +6,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class AccountServiceApplication {
+public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.config.name", "registration-server");
-		SpringApplication.run(AccountServiceApplication.class, args);
+		//System.setProperty("spring.config.name", "registration-server");
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
 }
